b94ea07d59abeeb10976aefb2c9237ff3f7c1b96
shubham2o <shubhamsharma.ha@gmail.com>
2022-09-22 23:58:16 +0530

    incomplete code

---
+++ script.js
@@ -0,0 +1,27 @@
+const quizData = [{
+    question: 'How old is Florin?',
+    a: '10',
+    b: '17',
+    c: '26',
+    d: '110',
+    correct: 'c'
+}, {
+    question: 'What is the most used programming language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd'
+}, {
+    question: 'Who is the president of USA?',
+    a: 'Florin Pop',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andrei',
+    correct: 'b'
+}, {
+    question: 'what does HTML stand for?'
+    a: 'HyperText Markup Language',
+
+
+}];
