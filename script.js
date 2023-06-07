8af2e11df8c9b0c54fe4e7c14e7e0a7eb03e3481
shubham2o <shubhamsharma.ha@gmail.com>
2023-06-07 18:23:43 +0530

    Added pointer cursor to input and label

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const quizData = [
-    {
+const quizData = [{
     question: 'How old is Florin?',
     a: '10',
     b: '17',
@@ -35,3 +34,35 @@ const quizData = [
     d: 'none of the above',
     correct: 'b'
 }];
+
+const questionEl = document.getElementById('question');
+const  a_text = document.getElementById('a_text');
+const  b_text = document.getElementById('b_text');
+const  c_text = document.getElementById('c_text');
+const  d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerHTML = currentQuizData.question;    
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+}
+
+submitBtn.addEventListener("click", () => {
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        // /TODO: Show results
+        alert("You Finished! Get yourself an Orange Lemonade.");
+    }
+});
